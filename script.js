87472878ccfac3353a580e9c0a2ace7097ad6d9e
Rohith M <rohithmerala@gmail.com>
2022-02-01 14:50:37 +0530

    V1.0

---
+++ script.js
@@ -0,0 +1,7 @@
+function Turn_On() {
+  document.getElementById("imgbulb").src = "https://s3-projectsassets.whjr.online/On+Bulb.png";
+}
+
+function Turn_Off() {
+  document.getElementById("imgbulb").src = "https://s3-projectsassets.whjr.online/Off+Bulb.png";
+}
